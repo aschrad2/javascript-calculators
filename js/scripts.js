89f487dff2c42aea2f5157f20cbc7c589f2b6b95
Student <example@example.com>
2019-02-13 19:38:06 -0800

    Added the backend implementation for all the buttons. Returns a response number based on what is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -27,10 +27,68 @@ $(document).ready(function() {
     $("#output").text(result);
   });
 
+// Uses a click handler to output the number 1 when the button 1 is clicked
   $("#one").click(function(event) {
     // debugger;
     event.preventDefault();
     response = "1";
     $("#numberCounter").text(response)
   });
+
+  $("#two").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "2";
+    $("#numberCounter").text(response)
+  });
+
+  $("#three").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "3";
+    $("#numberCounter").text(response)
+  });
+
+  $("#four").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "4";
+    $("#numberCounter").text(response)
+  });
+
+  $("#five").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "5";
+    $("#numberCounter").text(response)
+  });
+
+  $("#six").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "6";
+    $("#numberCounter").text(response)
+  });
+
+  $("#seven").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "7";
+    $("#numberCounter").text(response)
+  });
+
+  $("#eight").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "8";
+    $("#numberCounter").text(response)
+  });
+
+  $("#nine").click(function(event) {
+    // debugger;
+    event.preventDefault();
+    response = "9";
+    $("#numberCounter").text(response)
+  });
+
 });
