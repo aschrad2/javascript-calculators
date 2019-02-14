3683fec4e426c2971fecffe973e5768057577cc0
Student <example@example.com>
2019-02-13 20:27:50 -0800

    Refactored the grabbing of the button values. This is now done by using a jQuery function

--- js/scripts.js
+++ js/scripts.js
@@ -27,67 +27,15 @@ $(document).ready(function() {
     $("#output").text(result);
   });
 
+// We're attaching parameters to the html elements  and then we're grabbing them using the jQuery. We're targeting this instance of the clicked
+// and we're grabbing the value associated with that very click. Data('calc') in jQuery correlates to data-calc="1".
 // Uses a click handler to output the number 1 when the button 1 is clicked
-  $("#one").click(function(event) {
+  $(".calcbutton").click(function(event) {
     // debugger;
     event.preventDefault();
     response = "1";
-    $("#numberCounter").text(response)
-  });
 
-  $("#two").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "2";
-    $("#numberCounter").text(response)
-  });
-
-  $("#three").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "3";
-    $("#numberCounter").text(response)
-  });
-
-  $("#four").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "4";
-    $("#numberCounter").text(response)
-  });
-
-  $("#five").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "5";
-    $("#numberCounter").text(response)
-  });
-
-  $("#six").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "6";
-    $("#numberCounter").text(response)
-  });
-
-  $("#seven").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "7";
-    $("#numberCounter").text(response)
-  });
-
-  $("#eight").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "8";
-    $("#numberCounter").text(response)
-  });
-
-  $("#nine").click(function(event) {
-    // debugger;
-    event.preventDefault();
-    response = "9";
+    console.log($(this).data('calc'));
     $("#numberCounter").text(response)
   });
 
