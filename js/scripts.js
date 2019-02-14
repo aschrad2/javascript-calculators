69efeb58f3e5bc82b647b0e21bac41c467d10c3d
Student <example@example.com>
2019-02-13 19:12:58 -0800

    Has the backend implementation of subtraction

--- js/scripts.js
+++ js/scripts.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
-    var result = add(number1, number2);
+    var result = subtract(number1, number2);
     $("#output").text(result);
   });
 });
